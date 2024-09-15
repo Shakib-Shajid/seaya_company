f27e3b6362b1319e0966daf0cfa4ce2a0d850b37
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-15 21:18:32 +0600

    location and reach added with responsive in contact page

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -4,17 +4,16 @@ import React from 'react';
 const page = () => {
     return (
         <div>
-            <section className='pb-16 md:pb-0 md:pl-24 md:pt-24 mt-20 md:mt-0 ml-2 md:ml-0'>
+            <section className='md:p-16 mt-20 my-20 md:my-0 md:mt-10 lg:mt-24 ml-2 md:ml-8'>
                 <h3 className='text-3xl md:text-4xl lg:text-6xl'>Contact</h3>
-
             </section>
 
-            <section className='ax-w-full '>
-                <div className='md:pl-24 md:pt-24 ml-2 md:ml-0'>
+            <section className='max-w-full '>
+                <div className='md:pl-24 lg:pt-16 ml-2 md:ml-0'>
                     <hr className='border mb-5 border-gray-400 md:w-11/12' />
-                    <div className='flex gap-3 flex-col lg:flex-row md:w-11/12 my-10 lg:my-20'>
-                        <h3 className='text-3xl md:text-4xl lg:text-6xl w-full md:w-1/3'>Reach out</h3>
-                        <div className='w-full md:w-2/3'>
+                    <div className='flex gap-3 flex-col lg:flex-row md:w-11/12 my-10 md:my-5 lg:my-20'>
+                        <h3 className='text-3xl md:text-4xl lg:text-6xl w-full md:w-1/3 my-5'>Reach out</h3>
+                        <div className='w-full lg:w-2/3'>
                             <div className='flex flex-col lg:flex-row lg:gap-40 space-y-5'>
                                 <div>
                                     <p className='text-xl md:text-2xl'>Press inquiries</p>
@@ -31,7 +30,7 @@ const page = () => {
                                     <p className='text-base'>Click here</p>
                                 </div>
                             </div>
-                            <Image src="/reach.webp" width="300" height="300" alt="" className='w-full my-5 md:my-20' />
+                            <Image src="/reach.webp" width="300" height="300" alt="" className='w-full my-5 lg:my-20' />
                         </div>
                     </div>
 
@@ -39,13 +38,13 @@ const page = () => {
             </section>
 
             <section>
-                <div className='md:pl-24 md:pt-24 ml-2 md:ml-0'>
+                <div className='md:pl-24 md:pt-12 ml-2 md:ml-0'>
                     <hr className='border mb-5 border-gray-400 md:w-11/12' />
-                    <div className='flex flex-col md:flex-row gap-3 md:w-11/12 my-10 md:my-20'>
+                    <div className='flex flex-col lg:flex-row gap-3 md:w-11/12 my-10 md:my-5 lg:my-20'>
                         <h3 className='text-3xl md:text-4xl lg:text-6xl w-1/3 my-5'>Offices</h3>
-                        <div className='w-2/3'>
-                            <div className='grid grid-cols-1 md:grid-cols-2 gap-5'>
-                                <div className='w-80 md:w-96'>
+                        <div className='lg:w-2/3'>
+                            <div className='grid grid-cols-1 lg:grid-cols-2 gap-5'>
+                                <div className='w-80 md:w-full lg:w-96'>
                                     <Image src="/location_1.png" width="300" height="300" alt="" className='w-full' />
                                     <div className='space-y-2 text-lg'>
                                         <h3 className='text-xl font-bold mt-5'>Madrid</h3>
@@ -55,12 +54,12 @@ const page = () => {
                                     </div>
                                 </div>
 
-                                <div className='w-80 md:w-96'>
+                                <div className='w-80 md:w-full lg:w-96'>
                                     <Image src="/location_2.png" width="300" height="300" alt="" className='w-full' />
                                     <h3 className='text-xl font-bold mt-5'>London</h3>
                                 </div>
 
-                                <div className='w-80 md:w-96'>
+                                <div className='w-80 md:w-full lg:w-96'>
                                     <Image src="/location_3.png" width="300" height="300" alt="" className='w-full' />
                                     <div className='space-y-2 text-lg'>
                                         <h3 className='text-xl font-bold mt-5'>CDMX</h3>
