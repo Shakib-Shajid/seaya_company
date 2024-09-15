c9d3ff25dcc5b38b959a285ccc80ae47af605d8f
Shakib-Shajid <shakibalshajid1682@gmail.com>
2024-09-15 23:18:37 +0600

    contact pitch added

--- src/app/contact/page.jsx
+++ src/app/contact/page.jsx
@@ -1,4 +1,5 @@
 import Image from 'next/image';
+import Link from 'next/link';
 import React from 'react';
 
 const page = () => {
@@ -70,6 +71,125 @@ const page = () => {
                                 </div>
                             </div>
 
+                        </div>
+                    </div>
+                </div>
+            </section>
+
+
+            <section>
+                <div className='md:pl-24 md:pt-12 ml-2 md:ml-0'>
+                    <hr className='border mb-5 border-gray-400 md:w-11/12' />
+                    <div className='flex flex-col lg:flex-row gap-3 md:w-11/12 my-10 md:my-5 lg:my-20'>
+                        <h3 className='text-3xl md:text-4xl lg:text-6xl w-1/3 my-5'>Pitch</h3>
+                        <div className='lg:w-2/3 border border-gray-400'>
+                            <div className='flex justify-center mx-auto overflow-y-scroll h-screen my-5'>
+                                <div className='w-8/12'>
+                                    <h3 className='text-2xl font-bold mt-10'>Send us your project</h3>
+                                    <p>Please note that due to the high volume of submissions we receive, we may not be able to respond individually to every inquiry. Rest assured that each submission will be carefully reviewed by our team.
+                                        If your startup aligns with our investment focus and criteria, we will make every effort to reach out for further discussions</p>
+                                    {/* form */}
+                                    <div className='mb-10'>
+                                        <form action="">
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>Company Name <span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <input type="text" className="input input-bordered w-full" />
+                                            </label>
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>Website <span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <input type="text" className="input input-bordered w-full" />
+                                            </label>
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>One-liner of the company <span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <input type="text" className="input input-bordered w-full" />
+                                            </label>
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>Description of the opportunity <span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <textarea
+                                                    className="textarea textarea-bordered textarea-md w-full"></textarea>
+                                            </label>
+
+                                            <label className="form-control w-full">
+                                                <div className="label block">
+                                                    <p>What fund(s) are most relevant to you?<span className='text-red-500'>*</span></p>
+                                                    <p className='text-sm text-[#363A3A] mt-2'>You can find information about each fund (investment stages, ticket size, focus areas, etc.) at:</p>
+                                                    <Link href="" className='text-sm btn-link'><p>seaya.vc/seaya-ventures/</p></Link>
+                                                    <Link href="" className='text-sm btn-link'><p>seaya.vc/seaya-andromeda/</p></Link>
+                                                    <Link href="" className='text-sm btn-link'><p>seaya.vc/seaya-cathay-latam/</p></Link>
+                                                </div>
+                                                <div className='block'>
+                                                    <label className='flex items-center my-2 gap-1 cursor-pointer'>
+                                                        <input type="checkbox" className="checkbox checkbox-xs" />
+                                                        <span>Seaya Ventures</span>
+                                                    </label>
+                                                    <label className='flex items-center my-2 gap-1 cursor-pointer'>
+                                                        <input type="checkbox" className="checkbox checkbox-xs" />
+                                                        <span>Seaya Andromeda</span>
+                                                    </label>
+                                                    <label className='flex items-center my-2 gap-1 cursor-pointer'>
+                                                        <input type="checkbox" className="checkbox checkbox-xs" />
+                                                        <span>Seaya Cathay Latam</span>
+                                                    </label>
+                                                </div>
+
+                                            </label>
+
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>Contact person<span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <input type="text" className="input input-bordered w-full" />
+                                            </label>
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>Contact email <span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <input type="text" className="input input-bordered w-full" />
+                                            </label>
+
+
+                                            <label className="form-control w-full">
+                                                <div className="label block">
+                                                    <p>What fund(s) are most relevant to you?<span className='text-red-500'>*</span></p>
+                                                    <p className='text-sm text-[#363A3A] mt-2'>Please note that to successfully submit your project, you are required to add extra material (e.g. a detailed presentation)</p>
+                                                </div>
+                                                <input type="file" className="file-input file-input-bordered file-input-md w-full max-w-xs" />
+                                            </label>
+
+                                            <label className="form-control w-full">
+                                                <div className="label">
+                                                    <span>Additional comments <span className='text-red-500'>*</span></span>
+                                                </div>
+                                                <textarea
+                                                    className="textarea textarea-bordered textarea-md w-full"></textarea>
+                                            </label>
+
+                                            <label className="form-control w-full">
+                                                <div className="label block">
+                                                    <p>Disclaimer <span className='text-red-500'>*</span></p>
+                                                    <p className='text-sm text-[#363A3A] mt-2'>The personal data collected through this form will be processed by Seaya Capital Gestión SGEIC, S.A. with an address at C/ Alcalá 54, 28014 Madrid, based on your query and in order to provide you with a response.
+                                                        If you wish to exercise your rights of access, rectification, deletion, opposition, portability, limitation of processing, or those recognized by the regulations at any time, you can request it to the Data Protection Delegate at: lopd@seaya.vc. You can find complete information about the treatment of the data in https://seaya.vc/privacy-policy/</p>
+                                                </div>
+
+                                                <input type="checkbox" className="checkbox" />
+
+                                            </label>
+
+                                            <button className='btn btn-info text-white mt-5 mb-2'>Submit</button>
+                                            <p className='text-xs text-[#363A3A]'>Do not submit passwords through Airtable forms. Report malicious form</p>
+
+                                        </form>
+                                    </div>
+                                </div>
+                            </div>
                         </div>
                     </div>
                 </div>
